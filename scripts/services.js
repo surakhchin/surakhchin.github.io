c133eba9cfec3997ff396e46935c9cc2d4832cc2
Serge <urakhchin@gmail.com>
2018-04-16 18:48:30 -0500

    fixed link

--- scripts/services.js
+++ scripts/services.js
@@ -620,7 +620,7 @@ var samples=[            {
                           url: 'https://lasertag.cc',
                           date: '4/16/2018',
                           description:'A working progress MEAN stack web app that registers a hit from one client to another. Server-side use of nodejs, expess, socket.io, https, ubuntu ec2. Client-side use of angular, ajax, geolocation and device orientation web apis.',
-                          url2: 'http://airsoftonline.win/gun.html',
+                          url2: 'https://lasertag.cc/gun.html',
                           name2: 'Lasergun Mode'
                         },
                          {
