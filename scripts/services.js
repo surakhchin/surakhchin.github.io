38af967f526621694971a5032d9e1fd70e4a412f
Serge <urakhchin@gmail.com>
2018-04-16 18:50:52 -0500

    spelling on lasergun

--- scripts/services.js
+++ scripts/services.js
@@ -616,7 +616,7 @@ angular.module('confusionApp')
 var samples=[            {
                           _id:0,
                           name:'Lasertag.cc',
-                          image: 'images/lasergun3.PNG',
+                          image: 'images/lasergun3.png',
                           url: 'https://lasertag.cc',
                           date: '4/16/2018',
                           description:'A working progress MEAN stack web app that registers a hit from one client to another. Server-side use of nodejs, expess, socket.io, https, ubuntu ec2. Client-side use of angular, ajax, geolocation and device orientation web apis.',
